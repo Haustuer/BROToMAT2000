f2bf3ac7aa279179edb314cf1622e6d21e483020
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-01-28 22:14:25 -0800

    Encoder Teste

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,356 @@
 #include <Arduino.h>
 
+
 // Init Enviromnet Sensor
 #include <Adafruit_AHTX0.h>
 Adafruit_AHTX0 aht;
 #include <ElegantOTA.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h> 
+#include <ESP8266mDNS.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoHA.h>
 #include <Adafruit_NeoPixel.h>
 
+#include <Adafruit_MLX90614.h>
+
+#define ENCODER_SW D3
+#define ENCODER_CA D5
+#define ENCODER_CB D6
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
 unsigned long ota_progress_millis = 0;
+
+#define MAXTEMP 80
+#define MINTEMP 40
+
+#define MAXTIME 1000
+#define MINTIME 0
+
+double airTemp = 0;
+double irTemp = 0;
+double airHum = 0;
+
+double targetTemperatur = 24;
+unsigned int timeInSeconds = 20;
+int active = 0;
+
+enum menue
+{
+  setTemp,
+  setTime,
+  setState
+};
+
+enum menue Heater = setTemp;
 
 ESP8266WebServer server(80);
 
-void setup() {
- 
- 
+int hum;
+
+void setupIrTemp()
+{
+  while (!mlx.begin())
+  {
+    // Serial.println("Error connecting to MLX sensor. Check wiring.");
+    int counter = 0;
+    counter++;
+    if (counter % 1000 == 0)
+    {
+      Serial.print(".");
+    }
+  }
 }
 
-void loop() {
+
+
+void readIrTemp()
+{
+  irTemp = mlx.readObjectTempC();
+  // Serial.print("Temperature: ");
+  // Serial.print(tempObjF);
+  // Serial.println(" degrees F");
+}
+
+void sendState()
+{
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
+
+  airTemp = temp.temperature;
+  airHum = humidity.relative_humidity;
+  /*
+  Serial.print("Temperature: ");
+  Serial.print(airTemp);
+  Serial.println(" degrees F");
+  // Temp.setValue(temp.temperature);
+  Serial.print("Humidity: ");
+  Serial.print(airHum);
+  Serial.println("% rH");
+  */
+  // Hum.setValue(humidity.relative_humidity);
+}
+
+// int turnCounter=0;
+int increment = 0;
+
+IRAM_ATTR void ISR()
+{
+  bool CB = digitalRead(ENCODER_CB);
+ // bool CA = digitalRead(ENCODER_CA);
+  if (CB)
+  {
+    increment = +1;
+  }
+  else
+  {
+    increment = -1;
+  }
+}
+
+void setupEncoder()
+{
+
+  pinMode(ENCODER_SW, INPUT);
+  pinMode(ENCODER_CA, INPUT_PULLUP);
+  pinMode(ENCODER_CB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CA), ISR, RISING);
+}
+
+int16_t inputDelta = 0;             // Counts up or down depending which way the encoder is turned
+bool printFlag = false;             // Flag to indicate that the value of inputDelta should be printed
+
+
+void readEncoder() {
+    static uint8_t state = 0;
+    bool CLKstate = digitalRead(ENCODER_CA);
+    bool DTstate = digitalRead(ENCODER_CB);
+    switch (state) {
+        case 0:                         // Idle state, encoder not turning
+            if (!CLKstate){             // Turn clockwise and CLK goes low first
+                state = 1;
+            } else if (!DTstate) {      // Turn anticlockwise and DT goes low first
+                state = 4;
+            }
+            break;
+        // Clockwise rotation
+        case 1:                     
+            if (!DTstate) {             // Continue clockwise and DT will go low after CLK
+                state = 2;
+            } 
+            break;
+        case 2:
+            if (CLKstate) {             // Turn further and CLK will go high first
+                state = 3;
+            }
+            break;
+        case 3:
+            if (CLKstate && DTstate) {  // Both CLK and DT now high as the encoder completes one step clockwise
+                state = 0;
+                ++inputDelta;
+                printFlag = true;
+            }
+            break;
+        // Anticlockwise rotation
+        case 4:                         // As for clockwise but with CLK and DT reversed
+            if (!CLKstate) {
+                state = 5;
+            }
+            break;
+        case 5:
+            if (DTstate) {
+                state = 6;
+            }
+            break;
+        case 6:
+            if (CLKstate && DTstate) {
+                state = 0;
+                --inputDelta;
+                printFlag = true;
+            }
+            break; 
+    }
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Starting BROT O MAT 2000");
+  Serial.println("Initializing temperature and humidity sensor!");
+  if (!aht.begin())
+  {
+    Serial.println("Could not find Sensor? Check wiring");
+    while (1)
+      delay(10);
+  }
+  setupIrTemp();
+  setupEncoder();
+}
+
+unsigned long TS = 0;
+unsigned long DT = 1000;
+unsigned long fTS = 0;
+unsigned long fDT = 5000;
+
+bool EncoderLastState = true;
+unsigned long encoderTS = 0;
+unsigned long encoderDB = 10;
+void encoderCheckChange()
+{
+  bool newState = digitalRead(ENCODER_SW);
+  if (newState == EncoderLastState)
+  {
+    // nothing
+  }
+  else
+  {
+
+    if (millis() - encoderTS > encoderDB)
+    {
+      EncoderLastState = newState;
+      encoderTS = millis();
+      if (newState)
+      {
+        Serial.println("Push");
+
+        switch (Heater)
+        {
+        case setTemp:
+          Heater = setTime;
+          break;
+        case setTime:
+          Heater = setState;
+          break;
+        case setState:
+          Heater = setTemp;
+          break;
+
+        default:
+          break;
+        }
+      }
+      else
+      {
+        Serial.println("Release");
+      }
+    }
+  }
+}
+
+unsigned long encoderRoTS = 0;
+unsigned long encoderRoDB = 100;
+
+
+
+void stateMashine()
+{
+  if (increment != 0)
+  {
+    if (millis() - encoderRoTS > encoderRoDB)
+    {
+      encoderRoTS = millis();
+      // Serial.print("Master ");
+      // Serial.println(CA);
+      // Serial.print("Slave ");
+      // Serial.println(CB);
+        noInterrupts();
+      switch (Heater)
+      {
+      case setTime:
+
+        timeInSeconds += increment;
+        // timeInSeconds = constrain(timeInSeconds, MINTIME, MAXTIME);
+        Serial.print("NewTime: ");
+        Serial.println(timeInSeconds);
+
+        break;
+      case setTemp:
+        targetTemperatur += increment;
+        //  targetTemperatur = constrain(targetTemperatur, MINTEMP, MAXTEMP);
+        Serial.print("New Target Temp: ");
+        Serial.println(targetTemperatur);
+        break;
+      case setState:
+
+        if (increment > 0)
+        {
+          active = true;
+          Serial.println("Der Ofen an ");
+        }
+        else
+        {
+          active = false;
+          Serial.println("Der Ofen aus ");
+        }
+        break;
+
+      default:
+        Serial.println("error in state mashine");
+        break;
+      }
+    }
+  }
+  increment = 0;
+   interrupts();
+}
+
+
+void timer(){
+  if (active)
+    {  timeInSeconds--;
+      timeInSeconds = constrain(timeInSeconds, MINTIME, MAXTIME);
+    }
+    if (timeInSeconds<=0){
+      
+        active = false;
+
+   
+    }
+}
+
+void loop()
+{
+  encoderCheckChange();
+
+  if (millis() - TS > DT)
+  {
+    TS = millis();
+    sendState();
+    readIrTemp();
+    timer();
+  }
+  stateMashine();
+
+
+
+  if (millis() - fTS > fDT)
+  {
+    fTS = millis();
+    Serial.print(" Oven is ");
+    if (active)
+    {
+      Serial.print(" ON ");   
+      
+    }
+    else
+    {
+      Serial.print(" OFF ");
+    }
+
+    Serial.print(" Target temp: ");
+    Serial.print(targetTemperatur);
+    Serial.print(" Air temp: ");
+    Serial.print(airTemp);
+    Serial.print(" IR temp: ");
+    Serial.print(irTemp);
+    Serial.print(" Air Hum: ");
+    Serial.print(airHum);
+    Serial.print(" Seconds to go: ");
+    Serial.print(timeInSeconds);
+
+    Serial.println("");
+  }
   
 }
