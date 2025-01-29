1e61a33a316f6b45623dcade739c3f7bbfbdfe1d
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-01-28 19:14:36 -0800

    includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Init Enviromnet Sensor
+#include <Adafruit_AHTX0.h>
+Adafruit_AHTX0 aht;
+#include <ElegantOTA.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h> 
+#include <ESP8266WiFi.h>
+#include <ArduinoHA.h>
+#include <Adafruit_NeoPixel.h>
+
+unsigned long ota_progress_millis = 0;
+
+ESP8266WebServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ 
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
